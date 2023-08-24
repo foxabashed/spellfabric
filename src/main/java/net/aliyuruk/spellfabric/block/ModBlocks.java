d30898f3dcc92b0eeaac43337518c1b8cc7b2969
pizzawithdirt <muhammedaliyuruk@proton.me>
2023-08-24 12:40:41 +0300

    Added loot tables for existing blocks and made mossy stone drop experience

--- src/main/java/net/aliyuruk/spellfabric/block/ModBlocks.java
+++ src/main/java/net/aliyuruk/spellfabric/block/ModBlocks.java
@@ -6,6 +6,7 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.block.Material;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
@@ -13,10 +14,13 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 public class ModBlocks {
     public static final Block MOSSY_STONE = registerBlock("mossy_stone",
-            new Block(FabricBlockSettings.of(Material.MOSS_BLOCK).strength(1.5f).requiresTool()), ModItemGroup.BLOCKS);
+            new ExperienceDroppingBlock(FabricBlockSettings.of(Material.MOSS_BLOCK).strength(1.5f).requiresTool(),
+                    UniformIntProvider.create(1, 2)), ModItemGroup.BLOCKS);
+
     public static final Block DARK_PURPUR_BLOCK = registerBlock("dark_purpur_block",
             new Block(FabricBlockSettings.of(Material.DECORATION).strength(3.0f).requiresTool()), ModItemGroup.BLOCKS);
     public static final Block DARK_PURPUR_PILLAR = registerBlock("dark_purpur_pillar",
